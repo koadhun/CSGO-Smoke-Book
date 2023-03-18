ce8f1d34e1362db2c337b11be4a6f6ca963a3cb6
koadhun <koadhun@gmail.com>
2023-03-18 15:43:59 +0100

    Code cleaning, dynamic dom
    -Cleaned the script code
    -Added comments
    -implemented dynamic DOM creation in mapdetails.html

--- script.js
+++ script.js
@@ -1,15 +1,12 @@
 /* 
 TODOs:
--Clean the code (there are a lot of codes that might not be in use)
--Add comments
+-Clean style.css
 -Put the smokes folder into the images folder - modify src of the images. New src should start with images/smokes/...
--Load map parts into UI from Spot class dynamically
 -Create map spots for all maps.
 -Add tutorial video below the smoke lineups
 -Modify the smoke lineups UI to be scrollable like the map parts part of the UI.
 -Add a background image for the body
 */
-
 class Spot {
     constructor(mapName, spotTitle, spotName, spotImageNumber){
         this.mapName = mapName;
@@ -17,90 +14,57 @@ class Spot {
         this.spotName = spotName;
         this.spotImageNumber = spotImageNumber;
     }
-
-    getMapName() {
-        return mapName;
-    }
-
-    getSpotTitle() {
-        return spotTitle;
-    }
-
-    getSpotName() {
-        return spotName;
-    }
-
-    getSpotImageNumber() {
-        return spotImageNumber;
-    }
-
-    getImageSrc() {
-        return `smokes/${mapName}/${spotName}${spotImageNumber}.jpg`
-    }
 }
 
+/* To be able to determine which map was selected by the user.
+ * Information passed by index.js
+*/
 const params = new URLSearchParams(window.location.search)
 const index = params.get('index')
-const smokeVideoDiv = document.getElementById('smokeVideo')
+
+/* DOM elements to show smokePositions (smoke lineups) on top of the screen */
 const lineupHeader = document.getElementById('lineup-header')
+const detailsDiv = document.getElementById('detailsContent')
+const smokePositions = document.createElement('div')
+smokePositions.id = 'smokePositions'
 
-const body = document.querySelector('body')
+/* For tutorial video below smokePositions div (not included yet.) */
+const smokeVideoDiv = document.getElementById('smokeVideo')
+const iframe = document.getElementById('setupVideo')
+// Hard coded a video to test embedding
+//iframe.src = 'https://www.youtube.com/embed/OokgEC8AFoo'
+// detailsDiv.appendChild(smokeVideoDiv)
 
-const createDust2Spots = () => {
-    const bDoors = new Spot('dust2','B DOORS', 'b-doors', 4)
-    const bWindow = new Spot('dust2', 'B WINDOW', 'b-window', 4)
-    const longCorner = new Spot('dust2', 'LONG CORNER', 'long-corner', 4)
-    const longPushCt = new Spot('dust2', 'CT | FROM LONG', 'long-push-ct', 4)
-    const midToB = new Spot('dust2', 'MID TO B', 'mid-to-b', 4)
-    const pit = new Spot('dust2', 'PIT', 'pit', 4)
-    const shortFromLong = new Spot('dust2', 'SHORT | FROM LONG', 'short-from-long', 4)
-    const shortOneWay = new Spot('dust2', 'SHORT ONE WAY', 'short-one-way', 5)
-    const shortPushCt = new Spot('dust2', 'CT | FROM SHORT', 'short-push-ct', 5)
-    const tunnelMid = new Spot('dust2', 'TUNNEL | FROM MID', 'tunnel-mid', 5)
-    const xboxLowerTunnel = new Spot('dust2', 'XBOX | LOWER TUNNEL', 'xbox-lower-tunnel', 4)
-    const xboxTspawn = new Spot('dust2', 'XBOX | T SPAWN', 'xbox-t-spawn', 4)
-}
-let dust2Spots = createDust2Spots()
+/* To show map part elements below smokePositions and the tutorial video (tutorial video not included yet.) */
+const container = document.querySelector('.slider-container');
+const track = document.querySelector('.slider-track');
+const items = Array.from(document.querySelectorAll('.mapDetailCard'));
+const prevBtn = document.querySelector('.prev-button');
+const nextBtn = document.querySelector('.next-button');
 
-let maps = ['mirage', 'inferno', 'overpass', 'vertigo', 'nuke', 'ancient', 'dust2', 'anubis']
+
 let infernoSpots = []
 let overpassSpots = []
 let vertigoSpots = []
 let nukeSpots = []
 let ancientSpots = []
 let anubisSpots = []
+let dust2Spots = []
 
-
-/* 
-detailsContent
- -slider-wrapper
-   -slider-container
-   -button.prev-lineup-button
-   -smokePositions
-   -button.next-lineup-button
-*/
-const detailsDiv = document.getElementById('detailsContent')
-const smokePositions = document.createElement('div')
-smokePositions.id = 'smokePositions'
-// Code now will work only with hardcoded dust2 selection.
-// TODO: Get the map name from index.html selection, then load data depending on that.
-
-detailsDiv.appendChild(smokePositions)
-
-
-// detailsDiv.appendChild(smokeVideoDiv)
-const iframe = document.getElementById('setupVideo')
-
-// Hard coded a video to test embedding
-//iframe.src = 'https://www.youtube.com/embed/OokgEC8AFoo'
-
-// Whats new:
-
-/* 
-    Added JavaScript
-    Hard coded images, embedding for testing purposes.
-    Basic CSS (Final layout still in progress)
-*/
+const createDust2Spots = () => {
+    dust2Spots.push(new Spot('dust2','B DOORS', 'b-doors', 4))
+    dust2Spots.push(new Spot('dust2', 'B WINDOW', 'b-window', 4))
+    dust2Spots.push(new Spot('dust2', 'LONG CORNER', 'long-corner', 4))
+    dust2Spots.push(new Spot('dust2', 'CT | FROM LONG', 'long-push-ct', 4))
+    dust2Spots.push(new Spot('dust2', 'MID TO B', 'mid-to-b', 4))
+    dust2Spots.push(new Spot('dust2', 'PIT', 'pit', 4))
+    dust2Spots.push(new Spot('dust2', 'SHORT | FROM LONG', 'short-from-long', 4))
+    dust2Spots.push(new Spot('dust2', 'SHORT ONE WAY', 'short-one-way', 5))
+    dust2Spots.push(new Spot('dust2', 'CT | FROM SHORT', 'short-push-ct', 5))
+    dust2Spots.push(new Spot('dust2', 'TUNNEL | FROM MID', 'tunnel-mid', 5))
+    dust2Spots.push(new Spot('dust2', 'XBOX | LOWER TUNNEL', 'xbox-lower-tunnel', 4))
+    dust2Spots.push(new Spot('dust2', 'XBOX | T SPAWN', 'xbox-t-spawn', 4))
+}
 
 /*
 Indexes:
@@ -113,32 +77,108 @@ Indexes:
 7: dust2
 8: anubis
 */ 
-const loadImages = (imageSrc, numberOfPictures) => {
+
+/* Loading data only into the necessary array.
+ * Alerting the user if the selected map is not available yet.
+ * selectedIndex comes from the index coming from index.js 
+ */
+const createCorrectMapParts = (selectedIndex) => {
+    if(selectedIndex == 0) {
+        alert("Work in progess... Check back later!")
+        // createMirageSpots()
+    } else if(selectedIndex == 1) {
+        alert("Work in progess... Check back later!")
+        // createInfernoSpots()
+    } else if(selectedIndex == 2) {
+        alert("Work in progess... Check back later!")
+        // createOverpassSpots()
+    } else if(selectedIndex == 3) {
+        alert("Work in progess... Check back later!")
+        // createVertigoSpots()
+    } else if(selectedIndex == 4) {
+        alert("Work in progess... Check back later!")
+        // createNukeSpots()
+    } else if(selectedIndex == 5) {
+        alert("Work in progess... Check back later!")
+        // createAncientSpots()
+    } else if(selectedIndex == 6) {
+        createDust2Spots()
+    } else if(selectedIndex == 7) {
+        alert("Work in progess... Check back later!")
+        // createAnubisSpots()
+    } else {
+        alert("Something went wrong. Please try again later.")
+    }
+}
+
+createCorrectMapParts(index)
+
+/* Dynamically creating map parts cards 
+Class: .mapDetailCard, id: Spot.spotName
+Append each card to .slider-track
+*/
+const createMapPartDivs = (selectedMap) => {
+    selectedMap.forEach((map) => {
+        let cardDiv = document.createElement('div')
+        cardDiv.classList.add('mapDetailCard')
+        cardDiv.id = map.spotName
+
+        let spotImage = document.createElement('img')
+        spotImage.src = `smokes/${map.mapName}/${map.spotName}${map.spotImageNumber}.jpg`
+        cardDiv.appendChild(spotImage)
+
+        let spotName = document.createElement('p')
+        spotName.classList.add('positionName')
+        spotName.innerText = map.spotTitle
+
+        cardDiv.appendChild(spotName)
+
+        cardDiv.addEventListener('click', () => {
+            loadImages(map)
+        })
+
+        track.appendChild(cardDiv)
+    })
+}
+
+createMapPartDivs(dust2Spots)
+
+/*
+ * This function calls all the necessary functions to create the smoke lineup UI
+ * Receiving the map Spot from the createMapPartsDiv function.
+ */ 
+const loadImages = (map) => {
     lineupHeader.style = "display: block;"
     detailsDiv.style.display = 'block'
 
     removeChildElements()
-    createImages('dust2',imageSrc, numberOfPictures)
+    createImages(map)
     fullScreenElements()
 }
 
-const createImages = (map, imageSrc, numberOfPictures) => {
-    for(let i=1; i < (numberOfPictures+1); i++){
+/*
+ * This function creates the smoke lineup image elements then adds them to the correct container (smokePositions)
+ */
+const createImages = (map) => {
+    for(let i=1; i < (map.spotImageNumber+1); i++){
         let image = document.createElement('img')
         image.classList.add('smokeImage', 'fullscreen-image')
         try {
-            image.src = `smokes/${map}/${imageSrc}${i}.jpg`
-            console.log()
+            image.src = `smokes/${map.mapName}/${map.spotName}${i}.jpg`
         } catch (e) {
             continue;
         }
+
         smokePositions.appendChild(image)
     }
+    detailsDiv.appendChild(smokePositions)
 }
 
-
+/* 
+ * This function is responsible for handling event listeners regarding making the smoke lineup images full screen.
+ * Including click and keydown events.
+*/
 const fullScreenElements = () => {
-
     // get all the images on the page
     const images = document.querySelectorAll('.fullscreen-image')
     let currentImageIndex = 0
@@ -150,14 +190,14 @@ const fullScreenElements = () => {
 
     // function to toggle fullscreen mode
     function toggleFullScreen(index) {
-    if (!document.fullscreenElement) {
-        currentImageIndex = index
-        // if not in fullscreen mode, go into fullscreen mode
-        images[currentImageIndex].requestFullscreen()
-    } else {
-        // if in fullscreen mode, exit fullscreen mode
-        document.exitFullscreen()
-    }
+        if (!document.fullscreenElement) {
+            currentImageIndex = index
+            // if not in fullscreen mode, go into fullscreen mode
+            images[currentImageIndex].requestFullscreen()
+        } else {
+            // if in fullscreen mode, exit fullscreen mode
+            document.exitFullscreen()
+        }
     }
 
     // add keydown event listener to exit fullscreen mode when pressing "Escape" key
@@ -174,12 +214,12 @@ const fullScreenElements = () => {
             }
         }
     })
-
-     /* Modify the code so user can click on left-right arrow to step to the next-previous picture
-        and add keydown event listener for right and left arrow to do the same */
 }
 
-// Removing every child elements from the smokePositions div.
+/* Removing every child elements from the smokePositions div.
+ * In this case, the users will only see the selected map part smoke lineups
+ * instead of adding more non-relevant pictures to the div.
+ */
 const removeChildElements = () => {
     if(smokePositions.childElementCount > 0) {
         while(smokePositions.firstChild) {
@@ -188,46 +228,12 @@ const removeChildElements = () => {
     }
 }
 
-// 
-const mapPartSelected = () => {
-    const allMapPartCards = document.querySelectorAll('.mapDetailCard')
-    allMapPartCards.forEach(function(map) {
-        map.addEventListener('click', () => {
-            let numberOfPictures = 0
-            let mapsWith4Pictures = ['b-doors','b-window','long-corner','long-push-ct','mid-to-b',
-                                     'pit','short-from-long', 'xbox-lower-tunnel','xbox-t-spawn']
-
-            let mapsWith5Pictures = ['short-one-way','short-push-ct','tunnel-mid']
-
-            if(mapsWith4Pictures.includes(map.id)){
-                numberOfPictures = 4
-            } else if(mapsWith5Pictures.includes(map.id)){
-                numberOfPictures = 5
-            } else {
-                alert('Something went wrong. Please, try again later!')
-            }
-            // if id = 4 képes mapok, akkor numberofpicts: 4, másiknál 5
-            loadImages(map.id, numberOfPictures)
-
-            window.scrollTo({top: 0, behavior: 'smooth'})
-        })
-    })
-}
-
-mapPartSelected()
-
-const container = document.querySelector('.slider-container');
-const track = document.querySelector('.slider-track');
-const items = Array.from(document.querySelectorAll('.mapDetailCard'));
-const prevBtn = document.querySelector('.prev-button');
-const nextBtn = document.querySelector('.next-button');
-
-// rest of the JavaScript code
-
-
 let slidePosition = 0;
 let itemsVisible = 0;
 
+/*
+ * Calculating container and item width, in this case only the items that fits the screen will be visible.
+*/
 function updateWidth() {
   const containerWidth = container.getBoundingClientRect().width;
   const itemWidth = items[0].getBoundingClientRect().width;
@@ -248,11 +254,12 @@ function updateWidth() {
   track.style.width = `${trackWidth}px`;
 }
 
-
+// Click event listener on the 'Next' button. Scrolling to the next item on each click.
 nextBtn.addEventListener('click', () => {
     track.scrollBy({ left: 310, behavior: 'smooth' });
   });
-  
+
+// Click event listener on the 'Prev' button. Scrolling to the previous item on each click.
 prevBtn.addEventListener('click', () => {
     track.scrollBy({ left: -310, behavior: 'smooth' });
   });
